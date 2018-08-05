a668dbf3de3db3d96180691fbefb4d855136285f
Christopher Wood <cjwood032@gmail.com>
2018-08-05 21:29:38 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,32 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-   # GET /posts
+
+  # GET /posts
   # GET /posts.json
   def index
     @posts = Post.all
   end
-   # GET /posts/1
+
+  # GET /posts/1
   # GET /posts/1.json
   def show
   end
-   # GET /posts/new
+
+  # GET /posts/new
   def new
     @post = Post.new
   end
-   # GET /posts/1/edit
+
+  # GET /posts/1/edit
   def edit
   end
-   # POST /posts
+
+  # POST /posts
   # POST /posts.json
   def create
     @post = Post.new(post_params)
-     respond_to do |format|
+
+    respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
@@ -30,7 +36,8 @@ class PostsController < ApplicationController
       end
     end
   end
-   # PATCH/PUT /posts/1
+
+  # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
     respond_to do |format|
@@ -43,7 +50,8 @@ class PostsController < ApplicationController
       end
     end
   end
-   # DELETE /posts/1
+
+  # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
     @post.destroy
@@ -52,13 +60,15 @@ class PostsController < ApplicationController
       format.json { head :no_content }
     end
   end
-   private
+
+  private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.find(params[:id])
     end
-     # Never trust parameters from the scary internet, only allow the white list through.
+
+    # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :description)
+      params.require(:post).permit(:title, :description, :created_at, :updated_at, :image_url)
     end
 end
