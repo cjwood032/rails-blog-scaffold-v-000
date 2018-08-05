c9bee4f8123dde9475becce0ea08f26954e2b6cb
Christopher Wood <cjwood032@gmail.com>
2018-08-05 21:29:53 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180805211157) do
+ActiveRecord::Schema.define(version: 20180805212925) do
 
   create_table "apartments", force: :cascade do |t|
     t.string   "address"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20180805211157) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "image_url"
   end
 
 end
